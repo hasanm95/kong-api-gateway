281f210d6f33b2a3b29bffde4e9bcde2dbab01c3
hasanm95 <hsncool10@gmail.com>
2024-11-17 15:02:59 +0800

    init

--- services/service1/server.js
+++ services/service1/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`, req['headers']);
+  console.log(`Received request: ${req.method} ${req.url}`);
   next();
 });
 
