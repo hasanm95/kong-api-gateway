bc4e30dba3a4b6cecacb9d81ef0a9a503360a3e5
hasanm95 <hsncool10@gmail.com>
2024-11-17 17:35:40 +0800

    http log

--- services/service1/server.js
+++ services/service1/server.js
@@ -7,6 +7,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.raw({ type: '*/*', limit: '10mb' }));
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Root endpoint',
@@ -19,6 +22,25 @@ app.get('/api/service1', (req, res) => {
     message: 'Hello from Service 1',
     timestamp: new Date()
   });
+});
+
+app.post('/logs', (req, res) => {
+  // Log both raw and parsed body
+  console.log('Headers:', req.headers);
+  
+  if (Buffer.isBuffer(req.body)) {
+    console.log('Raw body:', req.body.toString());
+    try {
+      const jsonBody = JSON.parse(req.body.toString());
+      console.log('Parsed body:', jsonBody);
+    } catch (e) {
+      console.log('Could not parse body as JSON');
+    }
+  } else {
+    console.log('Body:', req.body);
+  }
+  
+  res.status(200).send('OK');
 });
 
 app.listen(port, () => {
