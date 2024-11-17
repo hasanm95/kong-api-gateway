8487fe354aecdf29197b1c57d3a13f553dcdab7d
hasanm95 <hsncool10@gmail.com>
2024-11-17 12:46:45 +0800

    cors

---
+++ services/service1/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`, req['headers']);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Root endpoint',
+    timestamp: new Date()
+  });
+});
+
+app.get('/api/service1', (req, res) => {
+  res.json({
+    message: 'Hello from Service 1',
+    timestamp: new Date()
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Service 1 listening at http://localhost:${port}`);
+});
